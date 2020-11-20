64ec086b53f3a34b137e492e4dc72bd0e738e00f
AlexAxl80 <alex.vaca6676@utc.edu.ec>
2020-11-19 19:24:50 -0500

    DiseñoV0

--- resources/views/dashboard/inicio.blade.php
+++ resources/views/dashboard/inicio.blade.php
@@ -1,4 +1,14 @@
 @extends('plantillaDashboard')
+@section('home-item')
+active
+@endsection
+@section('breadcrumb')
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item active" aria-current="page">Home</li>
+  </ol>
+</nav>
+@endsection
 @section('body')
 <h1 class="h3 mb-4 text-gray-800">Home</h1>
 @endsection
